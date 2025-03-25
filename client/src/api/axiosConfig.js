3fa7ad817d19c61741e8c3738cd1c2296c2fabbe
vishnu KP <vishnuto16@gmail.com>
2025-03-25 06:03:45 +0530

    commit

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: 'https://product-shopping-app.onrender.com/api', 
     headers: {
         'Content-Type': 'application/json',
     },
